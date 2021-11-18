a64874192c81538312fb3e9c4f57c81a80fd294c
banunarmi <banunarmi@gmail.com>
2021-11-18 14:34:21 +0530

    first commit

---
+++ Testex.java
@@ -0,0 +1,11 @@
+public class Testex {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for(int i=1; i<6;i++) {
+			System.out.println("#");
+			
+	      }
+	}
+}
